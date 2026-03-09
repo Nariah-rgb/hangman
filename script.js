fc59fa9bc63611c5e136fff0af4719a382ca7cca
Nariah <527nhenderson@frhsd.com>
2026-03-09 11:52:53 -0400

    Enhance Hangman game layout and styling with centered elements and improved CSS effects

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const wordBank = ['soft', 'cone', 'melt', 'cool', 'cake', 'milk', 'mint', 'cold', 'dish', 'bean'];
 
+let currentWord;
+let guessedLetter = [];
+let attempts = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
+
     startGame();
-}
+});
 
 function startGame() {
-
+    const randomIndex = Math.floor(Math.random() * wordBank.length);
+    randomWord = wordBank[randomIndex]
+    guessedLetter = [];
+    attempts = 0;
+    console.log("currentWord")
 }
+
+function displayWord() {
+    let display = "";
+    for (let i = 0; i < wordBank.length, i++) {
+        let letter = word.charAt(i);
+    }
+}
+
+
+
+
